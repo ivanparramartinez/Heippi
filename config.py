782b9913aa432605c10184bc2f72358b3b0a800c
Ivan Parra <nesivapama@gmail.com>
2021-01-19 00:47:28 -0500

    actualizacion 19 enero (1)

--- config.py
+++ config.py
@@ -6,18 +6,19 @@ class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Onemen2021@localhost/heippi"
+    SQLALCHEMY_DATABASE_URI = "postgres://hariktpwixnuwa:41f3f4dc5cc3051571b1acbf9501eb4a763aa93f97b61646e4e96f2069ebec67@ec2-3-230-106-126.compute-1.amazonaws.com:5432/d868akntnd7q88"
     SECURITY_PASSWORD_SALT = "ConfirmationEmail"
 
     # mail settings
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
 
     # gmail authentication
-    MAIL_USERNAME = "pruebasdesarrolloivan@gmail.com"
-    MAIL_PASSWORD = "pruebas2021"
+    MAIL_USERNAME = '300e0855b7add0'
+    MAIL_PASSWORD = 'cd4dd85b6b8b9f'
 
     # mail accounts
-    MAIL_DEFAULT_SENDER = 'pruebasdesarrolloivan@gmail.com'
+    MAIL_DEFAULT_SENDER = 'hospitalheippi@mail.com'
+    MAIL_FROM_NAME = 'Hospital Heippi'
